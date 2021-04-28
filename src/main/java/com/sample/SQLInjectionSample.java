059c6ed3c902605b58958da4a4a64727c4f00756
Ramesh Mahimalur <rmahimalur@nimbusconsulting.com>
2021-04-28 15:42:04 -0400

    fixing the vulnerabilities

--- src/main/java/com/sample/SQLInjectionSample.java
+++ src/main/java/com/sample/SQLInjectionSample.java
@@ -30,7 +30,8 @@ public class SQLInjectionSample {
                 + customerId
                 + "'";
         Connection c = getConn();
-        ResultSet rs = c.createStatement().executeQuery(sql);
+        //ResultSet rs = c.createStatement().executeQuery(sql);
+        ResultSet rs = stmt.executeQuery(query);
         while(rs.next()) {
             accountDTOS.add(new AccountDTO());
         }
